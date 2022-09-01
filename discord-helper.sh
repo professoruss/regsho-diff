bd171d59e1793de45ed197f2efa5360a8e1589a6
Russ Johnson <russ@angrypacket.com>
2022-09-01 15:31:31 -0700

    figure out where the hell we are

--- discord-helper.sh
+++ discord-helper.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 # get webhook url from external file
-source vars
+scriptdir=$(dirname "$(readlink -f "$BASH_SOURCE")")
+
+source ${scriptdir}/vars
 
 # run regsho script, replace tabs with spaces so json doesn't freak out later
-bash ./regsho.sh |expand -t 4 >> regsho.tmp
+bash ${scriptdir)/regsho.sh |expand -t 4 >> ${scriptdir}/regsho.tmp
 
 # add json header to file we will use with curl
-echo -n '{ "content": "```' > regsho2.tmp
+echo -n '{ "content": "```' > ${scriptdir}/regsho2.tmp
 
 # change internal field separator to newline
 IFS=$'\n'
 
 # iterate over lines of file as single line and add newline to end of each input line
-for i in `cat regsho.tmp`; do echo -n " $i\\n" >> regsho2.tmp; done
+for i in `cat ${scriptdir}/regsho.tmp`; do echo -n " $i\\n" >> ${scriptdir}/regsho2.tmp; done
 
 # add json footer
-echo '```"}' >> regsho2.tmp
+echo '```"}' >> ${scriptdir}/regsho2.tmp
 
 # post to discord
-curl -X POST ${DISCORDURL1} -H "Content-Type: application/json" -d @regsho2.tmp
+curl -X POST ${DISCORDURL1} -H "Content-Type: application/json" -d @${scriptdir}/regsho2.tmp
 
 # cleanup
-rm -f regsho2.tmp
-rm -f regsho.tmp
+rm -f ${scriptdir}/regsho2.tmp
+rm -f ${scriptdir}/regsho.tmp
 
 exit 0
